d134d8901eb16c67eafeeb4563f47d5225530535
pattmapoh <pattmapoh32@gmali.com>
2025-09-04 12:14:52 +0700

    migrate

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Students)
+class StudentsAdmin(admin.ModelAdmin):
+    list_display = ['stid', 'name_prefix', 'first_name', 'last_name', 'age']
+    search_fields = ['first_name', 'last_name']
+
+
+@admin.register(models.Subjects)
+class SubjectsAdmin(admin.ModelAdmin):
+    list_display = ['subject_code', 'subject_name', 'teacher_name']
+    search_fields = ['subject_code', 'subject_name', 'teacher_name']
--- webpage/models.py
+++ webpage/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+RREFIX_CHOICES = [
+    ('นาย', 'นาย'),  
+    ('นาง', 'นาง'),
+    ('นางสาว', 'นางสาว'),
+]
+
+class Students(models.Model):
+    stid = models.CharField(unique=True)
+    name_prefix = models.CharField(choices=RREFIX_CHOICES, max_length=10)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(default=18)
+    
+    def __str__(self):
+        return str(self.stid)
+
+
+class Subjects(models.Model):
+    subject_code = models.CharField(max_length=20, verbose_name="รหัสวิชา")
+    subject_name = models.CharField(max_length=100, verbose_name="ชื่อวิชา")
+    teacher_name = models.CharField(max_length=100, verbose_name="ชื่อผู้สอน")
+
+    def __str__(self):
+        return str(self.subject_code)
