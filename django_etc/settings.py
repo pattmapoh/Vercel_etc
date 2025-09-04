5397a6b45f28833851901ebf14c3d490c6347781
pattmapoh <pattmapoh32@gmali.com>
2025-09-04 16:25:58 +0700

    first commit

--- django_etc/settings.py
+++ django_etc/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'django_etc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.kjwrrmcenotjmavxjhnh',
+        'PASSWORD': 'pattmapoh2548',
+        'HOST': 'aws-1-ap-southeast-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
