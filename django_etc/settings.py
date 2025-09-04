46568cb73b4cf490c1ecedafc23c8a24373321a0
pattmapoh <pattmapoh32@gmali.com>
2025-09-04 16:56:07 +0700

    update STATIC_URL and STATICFILES_DIRS to use 'statics/' directory

--- django_etc/settings.py
+++ django_etc/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
